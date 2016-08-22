a78620929c9dc8dc73968caf9c75a815c67f0c4a
wgottschalk <will.gottschalk@gmail.com>
2016-08-21 20:56:18 -0700

    heap is implemented

---
+++ javascript/heap/heap.js
@@ -0,0 +1,73 @@
+function Heap (comparator = (a, b) => a - b) {
+    Object.defineProperty(this, 'size', {
+      get: function() {return this._heap.length - 1}
+    })
+    Object.defineProperty(this, 'heap', {
+      get: function() {return this._heap.slice(1)}
+    })
+    Object.defineProperty(this, 'min', {
+      get: function() {return this._heap.slice(1)[0]}
+    })
+    this.comp = comparator
+    this._heap = [null]
+  }
+
+  Heap.prototype = {
+    parentIndex(index) {return index >> 1},
+    leftIndex(index)   {return index << 1},
+    rightIndex(index)  {return (index << 1) + 1},
+    insert(element) {
+      this._heap.push(element)
+      this._bubbleUp(this.size)
+      return this
+    },
+    _bubbleUp(childIndex) {
+      const parentIdx = this.parentIndex(childIndex)
+      if (this.comp(this._heap[parentIdx], this._heap[childIndex]) > 0
+          && childIndex > 1) {
+        this._swap(parentIdx, childIndex)
+        this._bubbleUp(parentIdx)
+      }
+    },
+    _swap(a, b) {
+      let temp = this._heap[a]
+      this._heap[a] = this._heap[b]
+      this._heap[b] = temp
+    },
+    extractMin() {
+      const min = this.min
+      this._heap[1] = this._heap.pop()
+      this._bubbleDown(1)
+      return min
+    },
+    _bubbleDown(index) {
+      const leftIdx = this.leftIndex(index)
+      const rightIdx = this.rightIndex(index)
+      let minIdx
+
+      if (this.comp(this._heap[index], this._heap[leftIdx]) > 0 ) {
+        minIdx = leftIdx
+      }
+
+      if (this.comp(this._heap[index], this._heap[rightIdx]) > 0
+      &&  this.comp(this._heap[leftIdx], this._heap[rightIdx]) > 0) {
+        minIdx = rightIdx
+      }
+
+      if (minIdx) {
+        this._swap(index, minIdx)
+        this._bubbleDown(minIdx)
+      }
+
+    },
+    heapsort(array) {
+      array.forEach( node => this.insert(node))
+      const sorted = []
+      for (let i = 0; i < array.length; i++) {
+        sorted.push(this.extractMin())
+      }
+      return sorted
+    }
+  }
+
+module.exports = Heap
--- javascript/tests/heap_test.js
+++ javascript/tests/heap_test.js
@@ -7,28 +7,28 @@ describe("heap", () => {
 
   describe("#parent", () => {
     it("returns the parent element at the given index", () => {
-      const root = this.h.getParent(2)
-      const parent = this.h.getParent(8)
+      const root = this.h.parentIndex(2)
+      const parent = this.h.parentIndex(8)
       expect(root).to.equal(1)
       expect(parent).to.equal(4)
     })
 
     it("returns the root when getParent is called on the root index", () => {
-      const root = this.h.getParent(1)
+      const root = this.h.parentIndex(1)
       expect(root).to.equal(root)
     })
   })
 
   describe("#leftChild", () => {
     it("returns the index of the left child at a given index", () => {
-      const child = this.h.getLeftChild(1)
+      const child = this.h.leftIndex(1)
       expect(child).to.equal(2)
     })
   })
 
   describe("#rightChild", () => {
     it("returns the index of the right child at a given index", () => {
-      const child = this.h.getRightChild(1)
+      const child = this.h.rightIndex(1)
       expect(child).to.equal(3)
     })
   })
@@ -36,15 +36,33 @@ describe("heap", () => {
   describe("#insert", () => {
     it("inserts an item into the heap and maintains the integrity of the heap", () => {
       this.h.insert(3).insert(1).insert(5)
-      expect(this.h.getHeap()).to.eql([1, 5, 3])
+      expect(this.h.heap).to.eql([1, 3, 5])
     })
-    it("takes a list of items and bulk inserts them into the heap", () => {
-      this.h.insert(3, 1, 5)
-      expect(this.h.getHeap()).to.eql([1, 5, 3])
+  })
+
+  describe("#min", () => {
+    it("returns the min value of the heap as a property", () => {
+      const values = [7,4,8,9,2,3,1,6,0,5]
+      values.forEach( val => this.h.insert(val))
+      expect(this.h.min).to.equal(0)
     })
-    it("takes an array of items and bulk inserts", () => {
-      this.h.insert([3, 1, 5])       
-      expect(this.h.getHeap()).to.eql([1, 5, 3])
+  })
+
+
+  describe("#extractMin", () => {
+    it("deletes the minimun value and maintains the heap", () => {
+      const values = [7,4,8,9,2,0]
+      values.forEach( val => this.h.insert(val))
+      const min = this.h.extractMin()
+      expect(min).to.equal(0)
+      expect(this.h.heap).to.eql([2, 4, 8, 9, 7])
+    })
+  })
+
+  describe("#heapsort", () => {
+    it("sorts an aray of numbers", () => {
+      expect(this.h.heapsort([5,4,3,6,7,2,1,8,9,0]))
+        .to.eql([0,1,2,3,4,5,6,7,8,9])
     })
   })
   // describe("#heapify", () => {
